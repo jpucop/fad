21ba1507178893cee8928567801ccd077066039d
jkirton <jkirton@ucop.edu>
2025-04-07 10:58:28 -0700

    restruct proj files some

--- source-venv.bat
+++ source-venv.bat
@@ -6,10 +6,10 @@ set VENV_DIR=venv
 
 rem Check if requirements.txt exists
 if not exist "requirements.txt" (
-    echo ❌ Error: requirements.txt not found in %CD%.
+    echo Error: requirements.txt not found in %CD%.
     exit /b 1
 )
 
 rem Activate the virtual environment
-echo ✅ Activating virtual environment...
+echo Activating virtual environment...
 call %VENV_DIR%\Scripts\activate
